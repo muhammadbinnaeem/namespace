9de73d11bfbca24d36586491774c586f39d4fb29
Muhammad Bin Naeem <mbinnaeem20@gmail.com>
2020-03-24 06:30:40 -0700

    service coordinates mapping with markers

--- screens/Map/index.js
+++ screens/Map/index.js
@@ -1,23 +1,36 @@
-import  React, { useEffect } from 'react';
+import  React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { StyleSheet, View,Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView,{ Marker } from 'react-native-maps';
 import { GET_ALL_TASK } from './action';
 
 
 export default function Map() {
   const dispatch = useDispatch();
   const map = useSelector(state => state.map);
+  const [markers,setMarkers] = useState([])
   useEffect(() => {
     dispatch({ type: GET_ALL_TASK });
   }, []);
 
   useEffect(() => {
-    console.log(map.tasks);
+    let tempMarkers = [];
+    map.tasks.map((task, i) => 
+    {
+      task.address.location &&  tempMarkers.push({ 
+        latitude: task.address.location.coordinates[1],
+        longitude: task.address.location.coordinates[0]
+      })
+    })
+    setMarkers(tempMarkers);
   }, [map.tasks]);
   return (
     <View style={styles.container}>
-      <MapView style={styles.mapStyle}></MapView>
+      <MapView style={styles.mapStyle}>
+          {markers.map((marker, i) => 
+            ( <Marker key={i} identifier={`id${i}`} coordinate={marker} />) 
+          )}
+      </MapView>
     </View>
   );
 }
