96ea194a579d260d9ea3795655da2f0952f46b85
Muhammad Bin Naeem <mbinnaeem20@gmail.com>
2020-03-24 00:30:31 -0700

    Add task screen design

--- screens/AddTask/index.js
+++ screens/AddTask/index.js
@@ -1,11 +1,20 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 export default function AddTask() {
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-  
+    <ScrollView style={styles.container} >
+      <View style={styles.container}>
+        <Text style={styles.heading}>Add Task</Text>
+        <TextInput
+          placeholder={'Address'}
+          style={styles.input}
+        />
+        <Button
+          title={'Add Task'}
+        />
+      </View>
     </ScrollView>
   );
 }
@@ -14,10 +23,20 @@ export default function AddTask() {
 
 const styles = StyleSheet.create({
   container: {
+    padding:20,
+    paddingTop:50,
     flex: 1,
-    backgroundColor: '#fafafa',
   },
-  contentContainer: {
-    paddingTop: 15,
+  input: {
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 20,
+  },
+  heading: {
+    fontSize:20,
+    marginBottom: 20,
+    fontWeight:'bold'
   }
 });
