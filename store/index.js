cd36318d267de7974ae414ca376df5d5e90135ef
Muhammad Bin Naeem <mbinnaeem20@gmail.com>
2020-03-24 04:50:14 -0700

    redux dev tool integration

--- store/index.js
+++ store/index.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware, compose  } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 import { rootReducer, rootInitialState } from './reducer';
 import rootEpic from './epic';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = composeWithDevTools({ realtime: true, port: 19001 });
 const epicMiddleware = createEpicMiddleware();
 const store = createStore(
   rootReducer,
